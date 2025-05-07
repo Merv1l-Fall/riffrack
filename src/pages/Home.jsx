5f64f3878426c6cc1826a28dedc88bb44c93b37d
Vilmer Fall <Vilmer.fall@gmail.com>
2025-05-07 20:11:24 +0200

    essage for no producs and responsivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react"
 import Filter from "../components/Filter"
 import StoreProduct from "../components/StoreProduct"
-// import { products } from "../data/products"
 import useProductStore from "../store/productStore"
 import "./Home.css"
 
@@ -12,16 +11,19 @@ const Home = () => {
 		fetchProducts();
 	}, [fetchProducts]);
 	return(
-		<section>
+		<section className="home-section">
 			<h1>Products</h1>
 			<Filter />
 			<div className="products-container">
-			{filteredProducts.map((item) => (
-				<StoreProduct 
-				item={item}
-				key={item.id}
-				/>
-			))
+			{filteredProducts.length === 0 ? ( <h2 className="no-products">No products found</h2>) : (
+				filteredProducts.map((item) => (
+					<StoreProduct 
+					item={item}
+					key={item.id}
+					/>
+				))
+
+			)
 			}
 			</div>
 		</section>
